a6df52edf8ad46d2740818d9c8e82433bab2cddf
pvvm <pv.mizuno@hotmail.com>
2020-03-20 12:19:16 -0300

    Cria grafo vazio e libera

--- main.c
+++ main.c
@@ -15,6 +15,26 @@ Pedro Vitor Valença Mizuno - 17/0043665
 #include <stdlib.h>
 #include <string.h>
 
+/* Estrutura de cada uma das células adjacentes*/
+typedef struct Struct_Celula *Apontador;
+struct Struct_Celula {
+  int Vertice;
+  Apontador Prox;
+}; 
+typedef struct Struct_Celula Celula;
+
+/* Estrutura da lista */
+typedef struct TipoLista {
+  Apontador Primeiro, Ultimo;
+} TipoLista;
+
+/* Estrutura do grafo*/
+typedef struct TipoGrafo {
+  TipoLista Adj[62];    // Lista dos adjacentes
+  int NumVertices;
+  int NumArestas;
+} TipoGrafo;
+
 /* Função responsável pela transformação dos dados lidos no arquivo em inteiros, armazenados na matriz vetores*/
 void transforma_em_int(int vetores[159][2], FILE *arq) {
     char linha[100];
@@ -37,7 +57,6 @@ void transforma_em_int(int vetores[159][2], FILE *arq) {
     }
 }
 
-
 /* Função responsável pela leitura do arquivo */
 void le_arquivo(int vetores[159][2]) {
     FILE *arq;
@@ -51,16 +70,53 @@ void le_arquivo(int vetores[159][2]) {
     transforma_em_int(vetores, arq);
 }
 
+/* Função responsável por criar a lista de adjacentes */
+void cria_lista_vazia(TipoLista *Lista) {
+    Lista->Primeiro = (Apontador) malloc (sizeof(Celula));
+    Lista->Ultimo = Lista->Primeiro;
+    Lista->Primeiro->Prox = NULL;
+}
+
+/* Função responsável por criar o grafo vazio */
+void cria_grafo_vazio(TipoGrafo *Grafo) {
+    for(int i = 0; i < Grafo->NumVertices; i++)
+        cria_lista_vazia(&Grafo->Adj[i]);
+}
+
+/* Função responsável por desalocar a memória usada no grafo */
+void libera_grafo(TipoGrafo *Grafo) {
+    Apontador anterior, aux;
+    for(int i = 0; i < Grafo->NumVertices; i++) {
+        aux = Grafo->Adj[i].Primeiro->Prox;
+        free(Grafo->Adj[i].Primeiro);   // Libera o primeiro elemento
+        Grafo->Adj[i].Primeiro = NULL;
+        while(aux != NULL) {            // Libera os demais, até que não existam outros
+            anterior = aux;
+            aux = aux->Prox;
+            free(anterior);
+        }
+    }
+}
+
 int main() {
     int vetores[159][2];
     le_arquivo(vetores);
 
-    for(int i = 0; i < 159; i++) {
+/*    for(int i = 0; i < 159; i++) {
         for(int j = 0; j < 2; j++) {
             printf("%d ", vetores[i][j]);
         }
         printf("\n");
-    }
+    }*/
+
+    TipoGrafo Grafo;
+    
+    Grafo.NumVertices = 62;
+    Grafo.NumArestas = 159;
+
+    cria_grafo_vazio(&Grafo);
+
+    libera_grafo(&Grafo);
 
     return 0;
 }
